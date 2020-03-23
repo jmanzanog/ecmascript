6ccd48ba43880802f15db954988a737403af643f
josemanzano <j.manzano.g@hotmail.com>
2020-03-23 16:09:19 -0500

    -Formato con comillas francesas -Extraer atributos de un objeto con notacion de llaves -Unir varios arreglos con  la notacion ... -Scope de const, var, let

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,4 @@
+// default parameters
 function newFuntion(name = 'Jose', age = 25) {
     console.log(name, age)
 }
@@ -5,7 +6,35 @@ function newFuntion(name = 'Jose', age = 25) {
 newFuntion();
 newFuntion('Dago', 29);
 
+//interpolacion intead of concatenacion  con +
 let hola = 'Hello';
 let mundo = 'World';
 let holaMundo = `${hola} ${mundo}`;
 console.log(holaMundo);
+
+// hacer saltos de linea y concervar el formto de un rexto con las comillas francesas en ES6
+let lorem = `Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut.
+ Venenatis urna cursus eget nunc scelerisque viverra mauris in.
+  Sit amet venenatis urna cursus eget nunc scelerisque viverra mauris.
+   Sed cras ornare arcu dui vivamus arcu felis bibendum. 
+   Urna neque viverra justo nec ultrices dui sapien eget. 
+   Adipiscing bibendum est ultricies integer quis auctor elit.`;
+console.log(lorem);
+
+//dado un objeto puedo extraer sus atributos
+let person = {
+    name: 'Jose',
+    age: 50
+};
+let {name, age} = person;
+console.log(name, age);
+
+//puedo unir varios arreglos con la siguuiente notacion
+let lista1 = [1, 2, 3, 4, 5];
+let lista2 = [6, 7, 8, 9, 10];
+let lista3 = [...lista1, ...lista2, 11, 12, 13, 14, 15];
+console.log(lista3);
+
+// var tiene scope del archivo
+// let tiene scope solo al bloque de codigo donde se declara
+// const es inmutable
