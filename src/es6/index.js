0c1a45cc517a0bc687ca6eb54ea01dbca4f98d56
josemanzano <j.manzano.g@hotmail.com>
2020-03-23 15:37:50 -0500

    Default parameters and concatenation

---
+++ src/es6/index.js
@@ -0,0 +1,11 @@
+function newFuntion(name = 'Jose', age = 25) {
+    console.log(name, age)
+}
+
+newFuntion();
+newFuntion('Dago', 29);
+
+let hola = 'Hello';
+let mundo = 'World';
+let holaMundo = `${hola} ${mundo}`;
+console.log(holaMundo);
