4906e4f0b1e9954725623549a3c26fddaffcef15
josemanzano <j.manzano.g@hotmail.com>
2020-03-23 18:13:52 -0500

    lo nuevo de ES7

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+
+// ES7 incluyo .includes
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+if (numberList.includes(4)){
+    console.log('lo incluye');
+}else{
+    console.log('no lo incluye');
+}
+
+// ES7 incluyo ** para potencia
+
+console.log(4**4);
